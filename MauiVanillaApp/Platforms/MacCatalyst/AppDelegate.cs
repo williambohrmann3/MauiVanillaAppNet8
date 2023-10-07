c7812c1fad7d2ea0d89949d252bb72325bffdfc8
William Bohrmann <wbohrmann@esri.com>
2023-10-06 18:24:06 -0700

    Add project files.

---
+++ MauiVanillaApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiVanillaApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
